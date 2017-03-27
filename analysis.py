6fc50a33037c5092a2d2beb3b561d4317b9da262
uvafan <uvafan173@gmail.com>
2017-03-27 19:51:17 -0400

    fixed winning calculations bug

--- analysis.py
+++ analysis.py
@@ -11,14 +11,38 @@ def findHandStats(lines):
 	for ln in lines:
 		if ln[:8] == "Ignition":
 			hands += 1
-		for i in range(6, len(ln) - 17):
+		for i in range(6, len(ln)):
 			if ln[i:i + 3] == "ME]":
 				if ln[i + 4: i + 6] == "($": #Dollar amounts at start of each hand
-					outFor = findDollarAmount(ln, i + 6)
-					if inFor == 0:
-						inFor = outFor
-				elif  ln[i+4:i + 19] == ": Table deposit": #User making a table deposit
+					if inFor == 0: #start of session
+						inFor = findDollarAmount(ln, i + 6)
+						outFor = inFor
+					else: #check for bugs
+						realOutFor = findDollarAmount(ln, i + 6)
+						if round(outFor, 2) != realOutFor:
+							print("outFor = " + str(round(outFor, 2)) + " at line: " + ln)
+							exit(0)
+				elif ln[i + 4:i + 19] == ": Table deposit": #User making a table deposit
 					inFor += findDollarAmount(ln, i + 21)
+					outFor += findDollarAmount(ln, i + 21)
+				elif ln[i + 4:i + 10] == ": Bets": #User bets
+					outFor -= findDollarAmount(ln, i + 12)
+				elif ln[i + 4:i + 11] == ": Calls": #User calls
+					outFor -= findDollarAmount(ln, i + 13)
+				elif ln[i + 4:i + 12] == ": Raises": #User raises
+					outFor -= findDollarAmount(ln, i + 14)
+				elif ln[i + 4:i + 19] == ": All-in(raise)": #User raises all in
+					outFor -= findDollarAmount(ln, i + 21)
+				elif ln[i + 4:i + 12] == ": All-in": #User calls all in
+					outFor -= findDollarAmount(ln, i + 14)
+				elif ln[i + 4:i + 17] == ": Hand result": #User wins pot
+					outFor += findDollarAmount(ln, i + 19)
+				elif ln[i + 4:i + 36] == ": Return uncalled portion of bet": #uncalled raise returns
+					outFor += findDollarAmount(ln, i + 38)
+				elif ln[i + 4:i + 17] == ": Small Blind": #User's small blind
+					outFor -= findDollarAmount(ln, i + 19)
+				elif ln[i + 4:i + 15] == ": Big blind": #User's big blind
+					outFor -= findDollarAmount(ln, i + 17)
 	return [hands, outFor - inFor]
 
 def findDollarAmount(ln, startChar):
