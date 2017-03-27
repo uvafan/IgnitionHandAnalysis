7688fdee5990943207401ac8eee099f8b1b9b3b2
uvafan <uvafan173@gmail.com>
2017-03-27 17:26:34 -0400

    infra changes

--- analysis.py
+++ analysis.py
@@ -1,34 +1,38 @@
 def main():
-	fin = open("HistFiles/3-25_3.txt")
+	fin = open("Histories/1.txt")
 	lines = fin.readlines()
-	hands = findNumHands(lines)
-	winnings = findWinnings(lines)
-	print(winnings)
+	stats = findHandStats(lines)
+	print("In this session, you won $" + str(round(stats[1], 2)) + " in " + str(stats[0]) + " hands.") 
 
-def findNumHands(lines):
+def findHandStats(lines):
+	hands = 0
+	inFor = 0
+	outFor = 0
 	for ln in lines:
 		if ln[:8] == "Ignition":
 			hands += 1
+		for i in range(6, len(ln) - 17):
+			if ln[i:i + 3] == "ME]":
+				if ln[i + 4: i + 6] == "($": #Dollar amounts at start of each hand
+					outFor = findDollarAmount(ln, i + 6)
+					if inFor == 0:
+						inFor = outFor
+				elif  ln[i+4:i + 19] == ": Table deposit": #User making a table deposit
+					inFor += findDollarAmount(ln, i + 21)
+	return [hands, outFor - inFor]
 
-def findWinnings(lines):
-	start = 0
-	end = 0
-	for ln in lines:
-		for i in range(12, len(ln) - 17):
-			if ln[i:i + 7] == "[ME] ($":
-				startChar = i+7
-				endChar = startNumChar + 1
-				while ln[endChar] != ' ':
-					endChar += 1
-				convertToFloat = true
-				if ln[endChar - 3] != '.':
-					convertToFloat = false
-				if convertToFloat:
-					end = float(ln[startChar:endChar])
-				else:
-					end = int(ln[startChar:endChar])
-				if start == 0:
-					start = end
-	end = 
+def findDollarAmount(ln, startChar):
+	ret = 0
+	endChar = startChar + 1
+	while endChar < len(ln) - 1 and ln[endChar] != ' ':
+		endChar += 1
+	convertToFloat = True
+	if ln[endChar - 3] != '.':
+		convertToFloat = False
+	if convertToFloat:
+		ret = float(ln[startChar:endChar])
+	else:
+		ret = int(ln[startChar:endChar])
+	return ret
 
 main()
