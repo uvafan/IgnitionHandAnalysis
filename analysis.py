1726b05ac280bb0f8f0c98d25eaccafd4df5d400
uvafan <uvafan173@gmail.com>
2017-03-27 21:10:08 -0400

    added stats by position

--- analysis.py
+++ analysis.py
@@ -2,18 +2,24 @@ def main():
 	fin = open("Histories/1.txt")
 	lines = fin.readlines()
 	stats = findHandStats(lines)
-	print("In this session, you won $" + str(round(stats[1], 2)) + " in " + str(stats[0]) + " hands.") 
+	print("In this session, you won $" + str(round(stats[1], 2)) + " in " + str(stats[0]) + " hands.")
+	print("Net profits from each position:")
+	for i in range(0, 6):
+		print(numToPos(i) + ": $" + str(round(stats[2][i],2)))
 
 def findHandStats(lines):
 	hands = 0
 	inFor = 0
 	outFor = 0
+	positionProfits = [0] * 6
+	curPosNum = -1
 	for ln in lines:
 		if ln[:8] == "Ignition":
 			hands += 1
 		for i in range(6, len(ln)):
 			if ln[i:i + 3] == "ME]":
 				if ln[i + 4: i + 6] == "($": #Dollar amounts at start of each hand
+					curPosNum = posToNum(ln.split(' ')[2])
 					if inFor == 0: #start of session
 						inFor = findDollarAmount(ln, i + 6)
 						outFor = inFor
@@ -26,24 +32,42 @@ def findHandStats(lines):
 					inFor += findDollarAmount(ln, i + 21)
 					outFor += findDollarAmount(ln, i + 21)
 				elif ln[i + 4:i + 10] == ": Bets": #User bets
-					outFor -= findDollarAmount(ln, i + 12)
+					amt = findDollarAmount(ln, i + 12)
+					positionProfits[curPosNum] -= amt
+					outFor -= amt
 				elif ln[i + 4:i + 11] == ": Calls": #User calls
-					outFor -= findDollarAmount(ln, i + 13)
+					amt = findDollarAmount(ln, i + 13)
+					positionProfits[curPosNum] -= amt
+					outFor -= amt
 				elif ln[i + 4:i + 12] == ": Raises": #User raises
-					outFor -= findDollarAmount(ln, i + 14)
+					amt = findDollarAmount(ln, i + 14)
+					positionProfits[curPosNum] -= amt
+					outFor -= amt
 				elif ln[i + 4:i + 19] == ": All-in(raise)": #User raises all in
-					outFor -= findDollarAmount(ln, i + 21)
+					amt = findDollarAmount(ln, i + 21)
+					positionProfits[curPosNum] -= amt
+					outFor -= amt
 				elif ln[i + 4:i + 12] == ": All-in": #User calls all in
-					outFor -= findDollarAmount(ln, i + 14)
+					amt = findDollarAmount(ln, i + 14)
+					positionProfits[curPosNum] -= amt
+					outFor -= amt
 				elif ln[i + 4:i + 17] == ": Hand result": #User wins pot
-					outFor += findDollarAmount(ln, i + 19)
-				elif ln[i + 4:i + 36] == ": Return uncalled portion of bet": #uncalled raise returns
-					outFor += findDollarAmount(ln, i + 38)
+					amt = findDollarAmount(ln, i + 19)
+					positionProfits[curPosNum] += amt
+					outFor += amt
+				elif ln[i + 4:i + 36] == ": Return uncalled portion of bet": #uncalled bet/raise returns
+					amt = findDollarAmount(ln, i + 38)
+					positionProfits[curPosNum] += amt
+					outFor += amt
 				elif ln[i + 4:i + 17] == ": Small Blind": #User's small blind
-					outFor -= findDollarAmount(ln, i + 19)
+					amt = findDollarAmount(ln, i + 19)
+					positionProfits[curPosNum] -= amt
+					outFor -= amt
 				elif ln[i + 4:i + 15] == ": Big blind": #User's big blind
-					outFor -= findDollarAmount(ln, i + 17)
-	return [hands, outFor - inFor]
+					amt = findDollarAmount(ln, i + 17)
+					positionProfits[curPosNum] -= amt
+					outFor -= amt
+	return [hands, outFor - inFor, positionProfits]
 
 def findDollarAmount(ln, startChar):
 	ret = 0
@@ -58,5 +82,25 @@ def findDollarAmount(ln, startChar):
 	else:
 		ret = int(ln[startChar:endChar])
 	return ret
+
+def posToNum(pos):
+	return {
+		"Small": 0,
+		"Big": 1,
+		"UTG": 2,
+		"UTG+1": 3,
+		"UTG+2": 4,
+		"Dealer": 5	
+	}[pos]
+
+def numToPos(num):
+	return {
+		0: "Small",
+		1: "Big",
+		2: "UTG",
+		3: "UTG+1",
+		4: "UTG+2",
+		5: "Dealer"	
+	}[num]
 
 main()
